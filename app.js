a068d0ff779c2d13f8b5a28d38795ef601eb0a95
Rachel Conner <rconner@hondros.com>
2018-03-25 16:05:01 -0400

    Added logic for second rule of end time before 4am. Added functions to determine if times are before or after midnight and valid start or end times. added new test cases to check against and updated manual input section.

--- app.js
+++ app.js
@@ -10,31 +10,84 @@
         };
         $scope.tests = [
             {
-                startTime: new Date(2018, 2, 25, 12, 0),
+                startTime: new Date(2018, 2, 25, 18, 0),
+                endTime: new Date(2018, 2, 25, 17, 0),
                 expected: false
             },
             {
-                startTime: new Date(2018, 2, 25, 20, 45),
+                startTime: new Date(2018, 2, 25, 17, 0),
+                endTime: new Date(2018, 2, 25, 20, 0),
                 expected: true
             },
             {
-                startTime: new Date(2018, 2, 25, 16, 68),
+                startTime: new Date(2018, 2, 25, 19, 0),
+                endTime: new Date(2018, 2, 25, 2, 0),
                 expected: true
             },
             {
-                startTime: new Date(2018, 2, 25, 0, 43),
+                startTime: new Date(2018, 2, 25, 2, 0),
+                endTime: new Date(2018, 2, 25, 20, 0),
                 expected: false
             },
             {
-                startTime: new Date(2018, 2, 25, 22, 15),
+                startTime: new Date(2018, 2, 25, 16, 0),
+                endTime: new Date(2018, 2, 25, 3, 0),
+                expected: false
+            },
+            {
+                startTime: new Date(2018, 2, 25, 0, 0),
+                endTime: new Date(2018, 2, 25, 5, 0),
+                expected: false
+            },
+            {
+                startTime: new Date(2018, 2, 25, 0, 0),
+                endTime: new Date(2018, 2, 25, 4, 0),
                 expected: true
+            },
+            {
+                startTime: new Date(2018, 2, 25, 4, 0),
+                endTime: new Date(2018, 2, 25, 4, 0),
+                expected: true
+            },
+            {
+                startTime: new Date(2018, 2, 25, 4, 0),
+                endTime: new Date(2018, 2, 25, 2, 0),
+                expected: false
             }
         ];
 
         //first rule - babysitter cannot start before 5pm
-        $scope.testRule = function(input){
+        //Question: Would a start time ever be after midnight? Start times after or equal to midnight are currently invalid.
+
+        //second rule - babysitter leaves no later than 4am
+        //Question: If a babysitter shows up and leaves at the same time (if a job were cancelled, for example) is the end time valid? Currently all end times between 5pm - 4am are valid.
+        //Question: Since end time cannot be beyond 4am, can start time be up until 4am? If someone needed an emergency baby sitter, start times might be later? I've changed the start time to allow all the way up to 4am.
+
+        function afterFivePM(input){
             return input && input.getHours() >= 17;
-            //question: would a start time ever be after midnight? start times after or equal to midnight are currently invalid.
+        }
+
+        function beforeFourAM(input){
+            return input && input.getHours() <= 4;
+        }
+
+        function validTime(input){
+            return input && (afterFivePM(input) || beforeFourAM(input));
+        }
+
+        function validEnd(start, end){
+            if(start && end){
+                if((afterFivePM(end) && afterFivePM(start)) || (beforeFourAM(end) && beforeFourAM(start))){
+                    return end.getHours() >= start.getHours();
+                } else {
+                    return afterFivePM(start) && beforeFourAM(end);
+                }
+            }
+            //If start must be later than 5pm, end must be either the same or later than start. End can't be earlier than start.
+        }
+
+        $scope.testRule = function(start, end){
+            return start && end && validTime(start) && validTime(end) && validEnd(start, end);
         };
     }
 }());
